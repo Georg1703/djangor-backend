67c7bba7f1c73c9d4efc558e1d0a2ec3f09a148a
Gheorghe Mirca <giony.mirca@gmail.com>
2025-02-09 16:14:49 +0200

    Added new endpoint  and env example

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Invitation
 from .serializers import InvitationSerializer
@@ -7,3 +10,9 @@ from .serializers import InvitationSerializer
 class InvitationViewSet(viewsets.ModelViewSet):
     queryset = Invitation.objects.all()
     serializer_class = InvitationSerializer
+
+    @action(detail=True, methods=['get'], url_path='<uuid:guid>/')
+    def get_by_uuid(self, request, uuid=None):
+        invitation = get_object_or_404(Invitation, uuid=uuid)
+        serializer = self.get_serializer(invitation)
+        return Response(serializer.data)
