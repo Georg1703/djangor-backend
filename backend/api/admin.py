57f254a6089a8040e4e788cb9fc842c4eaf1aa45
Gheorghe Mirca <giony.mirca@gmail.com>
2025-02-12 22:02:27 +0200

    Added new column

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -7,13 +7,11 @@ from .models import Invitation
 
 @admin.register(Invitation)
 class InvitationAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'guest_1_name',
-        'guest_2_name',
-        'invitation_link'
-    )
+    list_display = ('id', 'guest_1_name', 'guest_2_name', 'invitation_link', 'name')
 
     def invitation_link(self, obj):
-        url = f"{HOST}{obj.id}/"
+        url = f'{HOST}{obj.id}/'
         return format_html('<a href="{0}" target="_blank">View Invitation</a>', url)
+
+    def name(self, obj):
+        return 'Mihai'
